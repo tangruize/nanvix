6593ce521df8330c75c291707a6f2bb0bb619af6
Ruize Tang <1466040111@qq.com>
2026-01-29 16:49:25 +0800

    [verus] Verification failed for kstack (verified:0, errors:0)

--- verus/kstack.rs
+++ verus/kstack.rs
@@ -507,7 +507,8 @@ impl KernelStack {
             self@.contains_addr(addr as int),
         ensures
             0 <= result < self.num_pages,
-            self@.page_start(result as int) <= addr as int < self@.page_end(result as int),
+            self@.page_start(result as int) <= addr as int,
+            addr as int < self@.page_end(result as int),
     {
         (addr - self.base_addr) / PAGE_SIZE
     }
