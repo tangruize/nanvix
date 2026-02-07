e539e67efce64037f0ef3dccba7251d20e68d736
Ruize Tang <1466040111@qq.com>
2026-02-07 23:55:40 +0800

    [verus] Verification succeeded for kernel::pm::thread::sleeping (verified:33, errors:0)

--- verus/split/kernel/pm/thread/sleeping.rs
+++ verus/split/kernel/pm/thread/sleeping.rs
@@ -290,6 +290,7 @@ impl SleepingThread {
             forall|a: int| result.spec_has_mutex(a) == self.spec_has_mutex(a),
             result.spec_drop_safe() == self.spec_drop_safe(),
             result.wf(),
+            result.spec_admission_time() >= 0,
     {
         ReadyThread::from_state(self.state)
     }
