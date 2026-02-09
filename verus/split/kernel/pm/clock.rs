6ab5f8309033774464ca1e464a46ed6358bd9e28
Ruize Tang <1466040111@qq.com>
2026-02-09 18:02:39 +0800

    [verus] Verification succeeded for kernel::pm::clock (verified:54, errors:0)

--- verus/split/kernel/pm/clock.rs
+++ verus/split/kernel/pm/clock.rs
@@ -533,7 +533,10 @@ pub fn standalone_ticks(timer: &TimerTicks) -> (result: u64)
 {
     let (major, minor): (u32, u32) = timer.get();
     proof {
+        assert(major <= u32::MAX);
+        assert(minor <= u32::MAX);
         assert(u32::MAX as nat * TimerTicks::MINOR_MODULUS() + u32::MAX as nat == u64::MAX as nat);
+        TimerTicks::lemma_nat_mul_le_mono(major as nat, u32::MAX as nat, TimerTicks::MINOR_MODULUS());
     }
     (major as u64) * 0x1_0000_0000u64 + (minor as u64)
 }
